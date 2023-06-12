bf05e6b461e3620962419eeca00d9c0bf434109a
rojasluis836@gmail.com <rojasluis836@gmail.com>
2023-06-12 10:30:08 -0400

    texto

--- application/views/admin/ventas.php
+++ application/views/admin/ventas.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Haciena Admin</title>
+  <title>hacienda Admin</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php $this->load->view('admin/css'); ?>
