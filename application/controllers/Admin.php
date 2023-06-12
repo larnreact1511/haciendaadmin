92f2fe4a09ad4dff7ef8cab05e6e619914af44f8
rojasluis836@gmail.com <rojasluis836@gmail.com>
2023-06-12 10:35:30 -0400

    pdf

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -222,10 +222,10 @@ class Admin extends CI_Controller {
 		$data['nro'] =$id;
 		$this->load->library('pdf');
 		//echo "<pre>";print_r($data); die;
-		$this->load->view('examples/facturapdf',$data);
-		//$html = file_get_contents($this->load->view('examples/pdfexample2',$data)); 
-		//$html = $this->load->view('examples/pdfexample2', $data, true);
-        //$this->pdf->createPDF($html, 'mypdf', false);
+		//$this->load->view('examples/facturapdf',$data);
+		//$html = file_get_contents($this->load->view('examples/facturapdf',$data)); 
+		$html = $this->load->view('examples/facturapdf', $data, true);
+        $this->pdf->createPDF($html, 'mypdf', false);
 	}
 	public function mypdf3($id)
 	{
